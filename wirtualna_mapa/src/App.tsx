fa752d1aa714c16a916568298a33ea65423340b8
kamilkarwowski <karwowski596@gmail.com>
2022-12-21 10:47:29 +0100

    navbar init

--- wirtualna_mapa/src/App.tsx
+++ wirtualna_mapa/src/App.tsx
@@ -3,11 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 import Login from './Login';
 import { SignUp } from './SignUp';
+import Nav from './components/Nav';
 
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <SignUp/>
     </div>
   );
