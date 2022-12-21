d8fdc6e065500b9fc9c7673d54d7f907b37bb77f
MateuszSamluk <mat.sam00@wp.pl>
2022-12-21 01:36:27 +0100

    adding footer to site

--- wirtualna_mapa/src/App.tsx
+++ wirtualna_mapa/src/App.tsx
@@ -3,14 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 import Login from './Pages/Login/Login';
 import { SignUp } from './Pages/SignUp/SignUp';
-
+import { Footer } from './Components/Footer/footer';
 
 function App() {
   return (
+    <React.Fragment>
     <div className="App">
       <SignUp/>
       {/* <Login/> */}
     </div>
+    <footer><Footer/></footer>
+    </React.Fragment>
   );
 }
 
