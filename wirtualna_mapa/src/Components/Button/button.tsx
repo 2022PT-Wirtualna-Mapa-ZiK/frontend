d6cf54cbae8ce70f3795d3fed59831accd9bef2f
Exodus <speeddriftt@gmail.com>
2023-04-08 17:55:27 +0200

    Fix button component so it works now

--- wirtualna_mapa/src/Components/Button/button.tsx
+++ wirtualna_mapa/src/Components/Button/button.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-
+import './button.css'
 
 type ButtonProps = {
-  link: string;
+  link?: string;
   text: string;
   className?: string;
+  form?: string;
 };
 
-const Button: React.FC<ButtonProps> = ({ link, text, className }) => {
+const Button: React.FC<ButtonProps> = ({ link, text, className, form }) => {
   const navigate = useNavigate();
-  navigate(link);
+
   return (
-    <button type="submit" className={className}>
+    <button type="submit" className={className} form={form} onClick={() => {
+      if(link) 
+        navigate(link);
+    }}>
       {text}
     </button>
   );
