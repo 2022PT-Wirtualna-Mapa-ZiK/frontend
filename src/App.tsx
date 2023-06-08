d0d7c1b2951d889342e6bedbd6d677f2fec9d8bf
PKuczynski <przemek.ku2000@gmail.com>
2023-06-08 16:54:54 +0200

    Remove imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import './App.css';
 
-import { SignUp } from './Pages/SignUp/SignUp';
-import { Footer } from './Components/Footer/footer';
-import Nav from './Components/Nav/Nav';
-import Home from './Pages/Home/Home';
-import WelcomePage from './Pages/WelcomePage/WelcomePage';
-import SignIn from './Pages/SignIn/SignIn';
-import CategoriesEmployers from './Pages/CategoriesEmployers/CategoriesEmployers';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+// import { SignUp } from './Pages/SignUp/SignUp';
+// import { Footer } from './Components/Footer/footer';
+// import Nav from './Components/Nav/Nav';
+// import Home from './Pages/Home/Home';
+// import WelcomePage from './Pages/WelcomePage/WelcomePage';
+// import SignIn from './Pages/SignIn/SignIn';
+// import CategoriesEmployers from './Pages/CategoriesEmployers/CategoriesEmployers';
+// import {
+//   createBrowserRouter,
+//   RouterProvider,
+// } from "react-router-dom";
 
 
 
